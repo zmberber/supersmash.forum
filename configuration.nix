da8fe205400c7a0731d981ef88f190ac16960243
Zeremonienmeister Berber <git@zmberber.com>
2024-04-05 21:04:40 +0200

    flakes and discourse

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,21 @@
   imports = [
     ./hardware-configuration.nix
   ];
-  services.openssh.enable = true;
-
+  services = {
+    openssh.enable = true;
+    discourse = {
+      enable = true;
+      admin = {
+        email = "admin@zmberber.com";
+        fullName = "Zeremonienmeister Berber";
+        username = "admin";
+        passwordFile = "/var/secrets/discourse-admin";
+      };
+    };
+    postgresql = {
+      package = pkgs.postgresql_13;
+    };
+  };
   boot = {
     loader = {
       systemd-boot.enable = true;
@@ -27,16 +40,24 @@
     };
   };
   
-  environment.systemPackages = map lib.lowPrio [
-    pkgs.curl
-    pkgs.gitMinimal
+  environment.systemPackages = with pkgs; [
+    vim
+    emacs-nox
   ];
+
+  security = {
+    acme = {
+      acceptTerms = true;
+      defaults.email = "certs@zmberber.com";
+    };
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGL4SqRZxHe+zziJ/8Q8nFfav5VfYj1zQUxnrHtrI2N4"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSGssxhQnkSyuqskIhiI7h4jIBkj2kMzNH6/LctK/2V"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDIb3uuMqE/xSJ7WL/XpJ6QOj4aSmh0Ga+GtmJl3CDvljGuIeGCKh7YAoqZAi051k5j6ZWowDrcWYHIOU+h0eZCesgCf+CvunlXeUz6XShVMjyZo87f2JPs2Hpb+u/ieLx4wGQvo/Zw89pOly/vqpaX9ZwyIR+U81IAVrHIhqmrTitp+2FwggtaY4FtD6WIyf1hPtrrDecX8iDhnHHuGhATr8etMLwdwQ2kIBx5BBgCoiuW7wXnLUBBVYeO3II957XP/yU82c+DjSVJtejODmRAM/3rk+B7pdF5ShRVVFyB6JJR+Qd1g8iSH+2QXLUy3NM2LN5u5p2oTjUOzoEPWZo7lykZzmIWd/5hjTW9YiHC+A8xsCxQqs87D9HK9hLA6udZ6CGkq4hG/6wFwNjSMnv30IcHZzx6IBihNGbrisrJhLxEiKWpMKYgeemhIirefXA6UxVfiwHg3gJ8BlEBsj0tl/HVARifR2y336YINEn8AsHGhwrPTBFOnBTmfA/VnP1NlWHzXCfVimP6YVvdoGCCnAwvFuJ+ZuxmZ3UzBb2TenZZOzwzV0sUzZk0D1CaSBFJUU3oZNOkDIM6z5lIZgzsyKwb38S8Vs3HYE+Dqpkfsl4yeU5ldc6DwrlVwuSIa4vVus4eWD3gDGFrx98yaqOx17pc4CC9KXk/2TjtJY5xmQ=="
   ];
+  
 
   system.stateVersion = "23.11";
 }
---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = inputs@{self, nixpkgs, ...}: {
+    nixosConfigurations.supersmash = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [./configuration.nix];
+    };
+  };
+            
+}
